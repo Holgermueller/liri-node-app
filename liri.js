db10cf6da031ee3c5204b2590aa056d2bcbb9400
Holger Mueller <9jholger@gmail.com>
2018-04-14 19:31:09 -0500

    add api imports

--- liri.js
+++ liri.js
@@ -1,3 +1,18 @@
+import { twitter } from "./keys";
+
 'use strict';
 
 require("dotenv").config();
+
+const spotify = new spotify(keys.spotify);
+const client = new twitter(keys.twitter);
+
+//liri must be able to take the following commmands
+//* `my-tweets`
+
+//* `spotify-this-song`
+
+//* `movie-this`
+
+//* `do-what-it-says`
+

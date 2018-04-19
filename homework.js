ab05993c22a9b9a6541b06de5f1034dc1cb99414
Holger Mueller <9jholger@gmail.com>
2018-04-18 19:29:13 -0500

    spotify apis work

--- homework.js
+++ homework.js
@@ -10,6 +10,21 @@ var client = new Twitter({
 var params = {screen_name: 'nodejs'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
-    console.log(tweets[0].text);
+    //console.log(tweets[0].text);
   }
+});
+
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: '',
+  secret: ''
+});
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
 });

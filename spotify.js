75403fec77cc626b6a1df769750ffd69f15ad78e
Holger Mueller <9jholger@gmail.com>
2018-04-20 22:56:01 -0500

    logged artist song album

--- spotify.js
+++ spotify.js
@@ -6,9 +6,15 @@ let Spotify = require('node-spotify-api');
 let keys = require('./keys.js');
 let spotify = new Spotify(keys.spotify);
 
-spotify.search({ type: 'track', query: 'lazarus' }, function(err, data) {
+spotify.search({ type: 'track', query: 'life on mars' }, function(err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     } 
-  console.log(data.tracks.items[0]); 
+  console.log(data.tracks.items[0].album);
+  console.log(data.tracks.items[0].artists[0].name); 
+  console.log(data.tracks.items[0].album.name);
+  //console.log(data.tracks.items[0]);
+  console.log(data.tracks.items[0].name);
   });
+
+//for song get: artist, song's name, preview link, album

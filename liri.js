9a1a324e9024c194fde349cd4526f79e50866449
Holger Mueller <9jholger@gmail.com>
2018-04-22 12:19:33 -0500

    twitter works in liri

--- liri.js
+++ liri.js
@@ -3,6 +3,7 @@
 require("dotenv").config();
 
 //requires
+let request = require("request");
 let spotify = require('node-spotify-api');
 let Twitter = require('twitter');
 let omdb = require('omdb');
@@ -10,19 +11,16 @@ let omdb = require('omdb');
 //bring in APIs from .env via keys
 let keys = require('./keys.js');
 //twitter keys
-const client = new twitter(keys.twitter);
+const client = new Twitter(keys.twitter);
 //spotify keys (for some reason, this is the only way I can get it to work)
-let spotify = new Spotify(keys.spotify);
-//omdb key
+//let spotify = new Spotify(keys.spotify);
+//omdb key?
 
+//fs
+let fs =require("fs");
 
 //GET tweets from my twitter acct.
-let params = {screen_name: 'HmBootcamp'};
-  client.get('statuses/user_timeline', params, function(error, tweets, response) {
-     if (!error) {
-      console.log(tweets);
-    }
-  });
+
 
 //GET 
 
@@ -31,6 +29,9 @@ let params = {screen_name: 'HmBootcamp'};
 //* `spotify-this-song`
 //* `movie-this`
 //* `do-what-it-says`
+
+let action = process.argv[2];
+let value = process.argv[3];
 
 switch (action) {
      case "my-tweets":
@@ -48,4 +49,24 @@ switch (action) {
      case "do-what-it-says":
      doWhatItSays();
      break;
+}
+
+//bring in Twitter function
+
+function myTweets() {
+  let params = {screen_name: 'HmBootcamp'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+     if (!error) {
+      console.log("Tweet: " + tweets[0].text + "\nCreated at: " + tweets[0].created_at);
+      console.log("Tweet: " + tweets[1].text + "\nCreated at: " + tweets[0].created_at);
+      console.log("Tweet: " + tweets[2].text + "\nCreated at: " + tweets[0].created_at);
+      console.log("Tweet: " + tweets[3].text + "\nCreated at: " + tweets[0].created_at);
+      console.log("Tweet: " + tweets[4].text + "\nCreated at: " + tweets[0].created_at);
+      console.log("Tweet: " + tweets[5].text + "\nCreated at: " + tweets[0].created_at);
+    }
+  });
+}
+
+function spotifyThisSong(){
+  
 }

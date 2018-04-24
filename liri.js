679c782d5397d13e1c2f90bfe5c468d98593e5b6
Holger Mueller <9jholger@gmail.com>
2018-04-23 19:48:35 -0500

    got spotify commands working

--- liri.js
+++ liri.js
@@ -4,17 +4,17 @@ require("dotenv").config();
 
 //requires
 let request = require("request");
-let spotify = require('node-spotify-api');
+let Spotify = require('node-spotify-api');
 let Twitter = require('twitter');
 let omdb = require('omdb');
 let fs =require("fs");
 
 //bring in APIs from .env via keys
-let keys = require('./keys.js');
+const keys = require('./keys.js');
 //twitter keys
 const client = new Twitter(keys.twitter);
 //spotify keys (for some reason, this is the only way I can get it to work)
-//let spotify = new Spotify(keys.spotify);
+const spotify = new Spotify(keys.spotify);
 
 //liri must be able to take the following commmands
 //* `my-tweets`
@@ -54,13 +54,26 @@ function myTweets() {
       console.log("Tweet: " + tweets[3].text + "\nCreated at: " + tweets[0].created_at);
       console.log("Tweet: " + tweets[4].text + "\nCreated at: " + tweets[0].created_at);
       console.log("Tweet: " + tweets[5].text + "\nCreated at: " + tweets[0].created_at);
+      console.log("Tweet: " + tweets[6].text + "\nCreated at: " + tweets[0].created_at);
+      console.log("Tweet: " + tweets[7].text + "\nCreated at: " + tweets[0].created_at);
+      console.log("Tweet: " + tweets[8].text + "\nCreated at: " + tweets[0].created_at);
+      console.log("Tweet: " + tweets[9].text + "\nCreated at: " + tweets[0].created_at);
+      console.log("Tweet: " + tweets[10].text + "\nCreated at: " + tweets[0].created_at);
+      console.log("Tweet: " + tweets[11].text + "\nCreated at: " + tweets[0].created_at);
+
     }
   });
 }
 
 //Spotify function
 function spotifyThisSong(){
-
+  spotify.search({ type: 'track', query: process.argv[3] }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+  console.log("Artist: " + data.tracks.items[0].artists[0].name + "\nTrack: " + data.tracks.items[0].name + 
+  "\nPreview Link: " + data.tracks.items[0].uri + "\nAlbum: " + data.tracks.items[0].album.name); 
+  });
 }
 
 //OMDB function
@@ -82,4 +95,8 @@ function movieThis() {
       console.log("Cast: " + JSON.parse(body).Actors);
     }
   });
+}
+
+function doWhatItSays() {
+  
 }

fb05080d582d41bb3fc43d00f1c24818420f5d75
Holger Mueller <9jholger@gmail.com>
2018-05-23 21:09:14 -0500

    finished last command

--- liri.js
+++ liri.js
@@ -1,96 +1,89 @@
-'use strict';
-
 require("dotenv").config();
-
 //requires
 let request = require("request");
 let Spotify = require('node-spotify-api');
 let Twitter = require('twitter');
 let omdb = require('omdb');
 let fs = require("fs");
-
 //bring in APIs from .env via keys
 const keys = require('./keys.js');
 const client = new Twitter(keys.twitter);
 const spotify = new Spotify(keys.spotify);
-
 //give liri commmands to take
 let command = process.argv[2];
 let searchTitle = process.argv[3];
 
 switch (command) {
-     case "my-tweets":
-     myTweets();
-     break;
+  case "my-tweets":
+    myTweets();
+    break;
 
-     case "spotify-this-song":
-     spotifyThisSong();
-     break;
+  case "spotify-this-song":
+    spotifyThisSong();
+    break;
 
-     case "movie-this":
-     movieThis();
-     break;
+  case "movie-this":
+    movieThis();
+    break;
 
-     case "do-what-it-says":
-     doWhatItSays();
-     break;
+  case "do-what-it-says":
+    doWhatItSays();
+    break;
 }
 
-//bring in Twitter function
+//  myTweets function
 function myTweets() {
-  fs.appendFile("log.txt", command + ", \n", function(err) {
+  fs.appendFile("log.txt", command + ", \n", function (err) {
     if (err) {
       return console.log(err);
     }
   });
 
-  client.get('statuses/user_timeline', {q: 'HmBootcamp', count: 20}, function(error, tweets, response) {
-     if (error) {
-       consol.log(error);
-     } else {
-       for (let i = 0; i < tweets.length; i++){
-       console.log("Tweet: " + tweets[i].text + "\nCreated at: " + tweets[i].created_at)
-       }
+  client.get('statuses/user_timeline', { q: 'HmBootcamp', count: 20 }, function (error, tweets, response) {
+    if (error) {
+      consol.log(error);
+    } else {
+      for (let i = 0; i < tweets.length; i++) {
+        console.log("Tweet: " + tweets[i].text + "\nCreated at: " + tweets[i].created_at)
+      }
     }
   });
 }
 
 //Spotify function
-function spotifyThisSong(){
-  fs.appendFile("log.txt", command + ", " + searchTitle + ", \n", function(err) {
+function spotifyThisSong() {
+  fs.appendFile("log.txt", command + ", " + searchTitle + ", \n", function (err) {
     if (err) {
       return console.log(err);
     }
   });
 
-  if(!searchTitle) {
+  if (!searchTitle) {
     searchTitle = 'The Sign';
   }
-  spotify.search({type: 'track', query: searchTitle}, function(err, data) {
+  spotify.search({ type: 'track', query: searchTitle }, function (err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
     let albumTrack = data.tracks.items;
-  console.log("Artist: " + albumTrack[0].artists[0].name + "\nTrack: " + albumTrack[0].name + 
-  "\nPreview Link: " + albumTrack[0].preview_url + "\nAlbum: " + albumTrack[0].album.name); 
+    console.log("Artist: " + albumTrack[0].artists[0].name + "\nTrack: " + albumTrack[0].name +
+      "\nPreview Link: " + albumTrack[0].preview_url + "\nAlbum: " + albumTrack[0].album.name);
   });
 }
 
 //OMDB function
 function movieThis() {
-  fs.appendFile("log.txt", command + ", " + searchTitle + ", \n", function(err) {
+  fs.appendFile("log.txt", command + ", " + searchTitle + ", \n", function (err) {
     if (err) {
       return console.log(err);
     }
   });
 
-  if(!searchTitle) {
+  if (!searchTitle) {
     searchTitle = "Mr. Nobody";
   }
   let queryUrl = "http://www.omdbapi.com/?t=" + searchTitle + "&y=&plot=short&apikey=trilogy";
-
-  request(queryUrl, function(error, response, body) {
-
+  request(queryUrl, function (error, response, body) {
     if (!error && response.statusCode === 200) {
       console.log("Movie title: " + JSON.parse(body).Title);
       console.log("Release Year: " + JSON.parse(body).Year);
@@ -106,17 +99,20 @@ function movieThis() {
 
 //fs function
 function doWhatItSays() {
-  fs.appendFile("log.txt", command + ", " + searchTitle + ", \n", function(err) {
+  fs.appendFile("log.txt", command + ", " + searchTitle + ", \n", function (err) {
     if (err) {
       return console.log(err);
     }
-  }) 
+  })
 
-  fs.readFile("random.txt", "utf8", function(error, data) {
-    if (error) {
-      return console.log(error);
+  fs.readFile("random.txt", "utf8", function (error, data) {
+    console.log(data);
+
+    let doWhatItSays = data.split(',');
+    if (doWhatItSays === 2) {
+      pick(doWhatItSays[0], doWhatItSays[1]);
+    } else if (doWhatItSays.length === 1) {
+      pick(doWhatItSays[0]);
     }
-    let iWantIt = data.split(',');
-    spotifyThisSong(iWantIt[1]);
   });
 }
